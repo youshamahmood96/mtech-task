056181bb6bade2acd551aa2e59a0a3e15a682b96
youshamahmood <youshamahmood96@gmail.com>
2021-04-07 04:56:47 +0600

    icon changed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import store, { persistor } from './Redux/Store/store'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css'
 
 ReactDOM.render(
@@ -19,7 +18,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
